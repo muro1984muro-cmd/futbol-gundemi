0d45de9976ee4f921dc569c5c27cdc593b8ae1fe
Hamza Salman <muro.1984.muro@gmail.com>
2026-08-16 16:09:23 +0300

    Implement WebView to load external URL

---
+++ app/src/main/java/com/futbolgundem/app/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.futbolgundem.app
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+
+        webView.webViewClient = WebViewClient()
+
+        webView.loadUrl(
+            "https://muro1984muro-cmd.github.io/futbol-gundemi/"
+        )
+
+        setContentView(webView)
+    }
+}
